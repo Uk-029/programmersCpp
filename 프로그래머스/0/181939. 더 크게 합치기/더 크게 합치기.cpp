fb8269380d77db259fefc39be04bb3b0a71528b8
Uk-029 <sohballmvp1@gmail.com>
2026-06-22 22:25:09 +0900

    [level 0] Title: 더 크게 합치기, Time: 0.01 ms, Memory: 4.7 MB -BaekjoonHub

--- 프로그래머스/0/181939. 더 크게 합치기/더 크게 합치기.cpp
+++ 프로그래머스/0/181939. 더 크게 합치기/더 크게 합치기.cpp
@@ -4,5 +4,15 @@
 using namespace std;
 
 int solution(int a, int b) {
-    return max(stoi(to_string(a)+to_string(b)),stoi(to_string(b)+to_string(a)));
+    int answer = 0;
+    
+    string A = to_string(a);
+    string B = to_string(b);
+    string sum1 = A + B;
+    string sum2 = B + A;
+
+    if(sum1 > sum2) answer = stoi(sum1);
+    else    answer = stoi(sum2);
+    
+    return answer;
 }
