c3f14bd38cb35839557f2403564dac843d23647c
Uk-029 <sohballmvp1@gmail.com>
2026-06-24 23:07:23 +0900

    [level 0] Title: 주사위 게임 2, Time: 0.02 ms, Memory: 5.2 MB -BaekjoonHub

---
+++ 프로그래머스/0/181930. 주사위 게임 2/주사위 게임 2.cpp
@@ -0,0 +1,18 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int solution(int a, int b, int c) {
+    
+    if(a != b && b!= c && c != a)   {
+        return  a + b + c;
+    }
+    else if((a == b && b != c) || (b == c && c != a) || (c == a && a != b))   {
+        return (a + b + c) * (a*a + b*b + c*c);
+    }
+    else if(a == b && b == c)   {
+        return (a + b + c) * (a*a + b*b + c*c) * (a*a*a + b*b*b + c*c*c);      
+    }
+        
+}
