e51264b4747359f311fc7dfa1840a4398c55d5c1
Uk-029 <sohballmvp1@gmail.com>
2026-06-27 22:50:07 +0900

    [level 0] Title: 수열과 구간 쿼리 3, Time: 0.19 ms, Memory: 4.89 MB -BaekjoonHub

---
+++ 프로그래머스/0/181924. 수열과 구간 쿼리 3/수열과 구간 쿼리 3.cpp
@@ -0,0 +1,13 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+vector<int> solution(vector<int> arr, vector<vector<int>> queries) {
+
+    for (const auto& q : queries) {
+        swap(arr[q[0]], arr[q[1]]);
+    }
+    
+    return arr;
+}
