58716d39b28005e20de8ec65a57be5f6236e0f14
Uk-029 <sohballmvp1@gmail.com>
2026-07-02 23:49:29 +0900

    [level 0] Title: 피자 나눠 먹기 (2), Time: 0.01 ms, Memory: 4.76 MB -BaekjoonHub

--- 프로그래머스/0/120815. 피자 나눠 먹기 （2）/피자 나눠 먹기 （2）.cpp
+++ 프로그래머스/0/120815. 피자 나눠 먹기 （2）/피자 나눠 먹기 （2）.cpp
@@ -4,10 +4,9 @@
 using namespace std;
 
 int solution(int n) {
-    int i{};
+    int i{};                    //피자판수
     for(;;) {
         i++;
-        if(n % 6 == 0)  return n / 6;
-        else if(6 * i % n == 0) return i;
+        if(6 * i % n == 0) return i;
     }
 }
