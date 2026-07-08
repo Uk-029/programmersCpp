2210908a2e6fb27ceb7567ce9b35346ef168f79a
Uk-029 <sohballmvp1@gmail.com>
2026-07-08 22:48:27 +0900

    [level 0] Title: 접두사인지 확인하기, Time: 0.01 ms, Memory: 4.89 MB -BaekjoonHub

---
+++ 프로그래머스/0/181906. 접두사인지 확인하기/접두사인지 확인하기.cpp
@@ -0,0 +1,14 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int solution(string my_string, string is_prefix) {
+    int answer = 0;
+    
+    if(my_string.size() >= is_prefix.size() &&
+      my_string.substr(0, is_prefix.size()) == is_prefix)
+        return 1;
+    
+    return answer;
+}
