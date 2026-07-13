bf5fda60710e665b5a582e0020019c01fb86a40c
Uk-029 <sohballmvp1@gmail.com>
2026-07-13 23:23:36 +0900

    [level 0] Title: 순서 바꾸기, Time: 0.01 ms, Memory: 4.7 MB -BaekjoonHub

---
+++ 프로그래머스/0/181891. 순서 바꾸기/순서 바꾸기.cpp
@@ -0,0 +1,15 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+vector<int> solution(vector<int> num_list, int n) {
+    vector<int> answer;
+    
+    answer.assign(num_list.begin() + n, num_list.end());
+    for(int i{}; i < n; i++)   {
+        answer.push_back(num_list[i]);
+    }
+    
+    return answer;
+}
