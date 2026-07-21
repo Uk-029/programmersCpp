fd1791ea3d82b67b493eb0bcaeeca839ab5b6275
Uk-029 <sohballmvp1@gmail.com>
2026-07-21 23:22:07 +0900

    [level 0] Title: 배열 만들기 6, Time: 6.03 ms, Memory: 33 MB -BaekjoonHub

---
+++ 프로그래머스/0/181859. 배열 만들기 6/배열 만들기 6.cpp
@@ -0,0 +1,18 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+vector<int> solution(vector<int> arr) {
+    vector<int> stk;
+    for(int i{}; i < arr.size(); i++)   {
+        if(stk.empty()) {
+            stk.push_back(arr[i]);
+        }
+        else if(!stk.empty())    {
+            stk.back() == arr[i] ? stk.pop_back() : stk.push_back(arr[i]);
+        }
+    }
+    if(stk.empty()) stk.push_back(-1);            
+    return stk;
+}
