01d0178dab8a9d8db9effe4db86513a5efbb10eb
Uk-029 <sohballmvp1@gmail.com>
2026-07-25 23:43:13 +0900

    [level 0] Title: 날짜 비교하기, Time: 0.01 ms, Memory: 4.82 MB -BaekjoonHub

--- 프로그래머스/0/181838. 날짜 비교하기/날짜 비교하기.cpp
+++ 프로그래머스/0/181838. 날짜 비교하기/날짜 비교하기.cpp
@@ -1,8 +1,7 @@
-#include <string>
 #include <vector>
 
 using namespace std;
 
 int solution(vector<int> date1, vector<int> date2) {
-    return date1 < date2 ? 1 : 0;
+    return date1 < date2;       //사전식 비교를 함. 즉 내부적으로 첫 번째부터 마지막 원소까지 동일한 원소끼리 비교함
 }
