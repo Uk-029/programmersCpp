a7f69f28fabd6e1e5c3ba11379400dbddda6b5bd
Uk-029 <sohballmvp1@gmail.com>
2026-07-25 23:24:43 +0900

    [level 0] Title: 정수 찾기, Time: 0.01 ms, Memory: 4.89 MB -BaekjoonHub

---
+++ 프로그래머스/0/181840. 정수 찾기/정수 찾기.cpp
@@ -0,0 +1,10 @@
+#include <vector>
+
+using namespace std;
+
+int solution(vector<int> num_list, int n) {
+    for(const int& num: num_list)   {
+        if(num == n)    return 1;
+    }
+    return 0;
+}
