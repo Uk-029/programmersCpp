c6046bb528751f0584fcd6ae81707a9b00c59a8b
Uk-029 <sohballmvp1@gmail.com>
2026-07-29 22:10:05 +0900

    [level 0] Title: 옷가게 할인 받기, Time: 0.01 ms, Memory: 5.07 MB -BaekjoonHub

---
+++ 프로그래머스/0/120818. 옷가게 할인 받기/옷가게 할인 받기.cpp
@@ -0,0 +1,14 @@
+#include <vector>
+
+using namespace std;
+
+int solution(int price) {
+    if (price >= 500000)
+        return price * 80 / 100;
+    else if (price >= 300000)
+        return price * 90 / 100;
+    else if (price >= 100000)
+        return price * 95 / 100;
+    else
+        return price;
+}
