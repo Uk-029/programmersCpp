b261208d4cc010ccf62f578809bcc5605ded7430
Uk-029 <sohballmvp1@gmail.com>
2026-07-30 22:38:06 +0900

    [level 0] Title: 문자 반복 출력하기, Time: 0.01 ms, Memory: 4.82 MB -BaekjoonHub

---
+++ 프로그래머스/0/120825. 문자 반복 출력하기/문자 반복 출력하기.cpp
@@ -0,0 +1,14 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+string solution(string my_string, int n) {
+    string answer = "";
+    
+    for(const char& c: my_string) {
+        answer.append(n, c);
+    }
+    
+    return answer;
+}
