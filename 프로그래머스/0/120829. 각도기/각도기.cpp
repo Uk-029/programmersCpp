39c2841526aa6932708b3bacf8f3ce49c2960217
Uk-029 <sohballmvp1@gmail.com>
2026-07-31 22:13:54 +0900

    [level 0] Title: 각도기, Time: 0.00 ms, Memory: 4.95 MB -BaekjoonHub

---
+++ 프로그래머스/0/120829. 각도기/각도기.cpp
@@ -0,0 +1,14 @@
+#include <vector>
+
+using namespace std;
+
+int solution(int angle) {
+    if(angle == 180)
+        return 4;
+    else if(angle > 90)
+        return 3;
+    else if(angle == 90)
+        return 2;
+    else if(angle > 0)
+        return 1;
+}
