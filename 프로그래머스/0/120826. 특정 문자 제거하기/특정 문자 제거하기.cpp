78dd443ca0670e930f2273db7a4c329663c082ee
Uk-029 <sohballmvp1@gmail.com>
2026-07-31 22:08:22 +0900

    [level 0] Title: 특정 문자 제거하기, Time: 0.01 ms, Memory: 4.89 MB -BaekjoonHub

---
+++ 프로그래머스/0/120826. 특정 문자 제거하기/특정 문자 제거하기.cpp
@@ -0,0 +1,13 @@
+#include <string>
+
+using namespace std;
+
+string solution(string my_string, string letter) {
+    string answer = "";
+    
+    for(const char& c: my_string)   {
+        if(c != letter[0]) answer += c;
+    }
+    
+    return answer;
+}
