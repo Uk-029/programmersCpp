cccd4106e94d42a8a97936d7fba17ca6b3992633
Uk-029 <sohballmvp1@gmail.com>
2026-08-10 21:24:21 +0900

    [level 0] Title: 문자열 정렬하기 (1), Time: 0.01 ms, Memory: 4.95 MB -BaekjoonHub

---
+++ 프로그래머스/0/120850. 문자열 정렬하기 （1）/문자열 정렬하기 （1）.cpp
@@ -0,0 +1,17 @@
+#include <string>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+vector<int> solution(string my_string) {
+    vector<int> answer;
+    for(const char& c: my_string)   {
+        if(c >= '0' && c <= '9')    {
+            answer.emplace_back(c - '0');
+        }
+    }
+    sort(answer.begin(), answer.end());
+    
+    return answer;
+}
