b6bcdcdd02bf78a15cbee30486c7c00f22082609
Uk-029 <sohballmvp1@gmail.com>
2026-08-19 22:25:28 +0900

    [level 0] Title: 머쓱이보다 키 큰 사람, Time: 0.01 ms, Memory: 4.95 MB -BaekjoonHub

---
+++ 프로그래머스/0/120585. 머쓱이보다 키 큰 사람/머쓱이보다 키 큰 사람.cpp
@@ -0,0 +1,14 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int solution(vector<int> array, int height) {
+    int answer = 0;
+    for(const int& arr: array)  {
+        if(arr > height)    {
+            answer++;
+        }
+    }
+    return answer;
+}
