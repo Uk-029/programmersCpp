833843b16342a9e1f3efb1150bf6bbfcf553c230
Uk-029 <sohballmvp1@gmail.com>
2026-09-07 23:08:25 +0900

    [level 1] Title: 핸드폰 번호 가리기, Time: 0.01 ms, Memory: 4.64 MB -BaekjoonHub

--- 프로그래머스/1/12948. 핸드폰 번호 가리기/핸드폰 번호 가리기.cpp
+++ 프로그래머스/1/12948. 핸드폰 번호 가리기/핸드폰 번호 가리기.cpp
@@ -4,14 +4,9 @@
 using namespace std;
 
 string solution(string phone_number) {
-    string answer = "";
-    
     for(int i{}; i < phone_number.size() - 4; i++)  {
-        answer += '*';
-    }
-    for(int i = answer.size(); i < phone_number.size(); i++)    {
-        answer += phone_number[i];
+        phone_number[i] = '*';
     }
     
-    return answer;
+    return phone_number;
 }
